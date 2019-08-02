af3cfc79808021c750c88b54b0dbe76054051c7f
Jordan Ketterling <jordanketterling@outlook.com>
2019-08-02 13:29:52 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Bringer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
